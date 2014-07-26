0fef2248c87957651a1adca69e9fc6c1318af4b9
mosaicer <scarlet_clover804@yahoo.co.jp>
2014-07-26 13:39:13 +0900

    version4.1
    厳密ではない文字の比較、通信エラー時の処理

--- Nico_Search_Blocker.user.js
+++ Nico_Search_Blocker.user.js
@@ -5,7 +5,7 @@
 // @description 各動画のタグ一覧を表示するボタンを追加し、タグ・キーワード検索のブロックを可能にする
 // @include     http://www.nicovideo.jp/tag/*
 // @include     http://www.nicovideo.jp/search/*
-// @version     4.0
+// @version     4.1
 // @grant       GM_xmlhttpRequest
 // @grant       GM_registerMenuCommand
 // @grant       GM_getValue
@@ -17,14 +17,15 @@ $(function () {
       // DBから取得する系処理
   var DBInit = function() {
         this.flagArray = {
-          "blcTags_flag": ["タグでの動画のブロックを", {}],
+          "blcTags_flag": ["タグでの動画のブロックを", {}], // オブジェクトは、キー:フラグ名+"Change",値:メニューの関数
           "blcKeywords_flag": ["キーワードでの動画のブロックを", {}],
           "unwantedVid_flag": ["検索対象外の動画のブロックを", {}],
           "removedVid_flag": ["削除済みの動画のブロックを", {}],
-          "cacheLoad_flag": ["キャッシュを使った読み込みを", {}]
+          "cacheLoad_flag": ["キャッシュを使った読み込みを", {}],
+          "strictComp_flag": ["文字列比較の厳格化を", {}]
         };
         this.blcNameArray = {
-          "blc_tags": [[], ["|タグ▼", "|タグ▲"]],
+          "blc_tags": [[], ["|タグ▼", "|タグ▲"]], // 配列にはブロックリストから取得した値が格納される
           "blc_keywords": [[], ["キーワード▼", "キーワード▲"]]
         };
         this.msgFlag = false;
@@ -42,9 +43,10 @@ $(function () {
       btnSetting,
       // ロード終了時に行う系処理
       LoadCheck = function() {
-        this.tagsList = {};
+        this.tagsList = {}; // オブジェクトは、キー:動画のID,値:各動画のタグ(配列)orタイトル
         this.titlesList = {};
         this.blcLtrTemp = "";
+        this.errCount = 0;
       },
       loadFeatures,
       // for-inで使う変数
@@ -97,20 +99,79 @@ $(function () {
   };
 
   LoadCheck.prototype = {
+    // HTTPリクエストのエラー時
+    errChecker: function(res) {
+      if (this.errCount === 0) {
+        this.errCount = 1;
+        alert("エラーが発生しました(" + this.errCount + ")。次回以降はコンソールに記録されます。\n" + "\nレスポンスの本体:\n" + res.responseText + "\nリクエストの状態: " + res.readyState + "\nレスポンスに含まれるHTTPヘッダー:\n" + res.responseHeaders + "\nレスポンスのHTTPエラーコード: " + res.status + "\nHTTPステータステキスト: " + res.statusText + "\nリダイレクト先のURL: " + res.finalUrl);
+        alert("連続してエラーが発生する場合は、ニコニコ動画やニコニコ動画APIの稼働状況をご確認ください。");
+      } else {
+        this.errCount++;
+        console.log("エラーが発生しました(" + this.errCount + ")。\n" + "\nレスポンスの本体:\n" + res.responseText + "\nリクエストの状態: " + res.readyState + "\nレスポンスに含まれるHTTPヘッダー:\n" + res.responseHeaders + "\nレスポンスのHTTPエラーコード: " + res.status + "\nHTTPステータステキスト: " + res.statusText + "\nリダイレクト先のURL: " + res.finalUrl);
+      }
+    },
     // 完全一致で比較(タグ・文字列追加時・検索対象)
     completeComp: function(targetLtr) {
-      if (typeof targetLtr !== "undefined" && targetLtr === this.clickObj && this.blcFlag === 0) {
-        this.blcFlag = 1;
-      } else if (this.chkFlag === 0 && this.clickObj === firstSetting.searchTag) {
-        this.chkFlag = 1;
+      // ブロック処理
+      if (typeof targetLtr !== "undefined") {
+        // 厳密に比較
+        if (this.blcFlag === 0 && GM_getValue("strictComp_flag") === true && this.clickObj === targetLtr) {
+          this.blcFlag = 1;
+        }
+        // 全角半角を気にしない
+        else if (this.blcFlag === 0 && GM_getValue("strictComp_flag") === false) {
+          this.clickObj = this.clickObj.toOneByteAlphaNumeric(); // どちらも全て半角にして統一
+          targetLtr = new RegExp("^" + preg_quote(targetLtr.toOneByteAlphaNumeric(), "/") + "$", "i"); // 大文字小文字を区別しない
+          if (this.clickObj.match(targetLtr)) {
+            this.blcFlag = 1;
+          }
+        }
+      }
+      // 検索対象が含まれているかのチェック
+      else {
+        // 厳密に比較
+        if (this.chkFlag === 0 && GM_getValue("strictComp_flag") === true && this.clickObj === firstSetting.searchTag) {
+          this.chkFlag = 1;
+        }
+        // 全角半角を気にしない
+        else if (this.chkFlag === 0 && GM_getValue("strictComp_flag") === false) {
+          this.clickObj = this.clickObj.toOneByteAlphaNumeric();
+          if (this.clickObj.match(firstSetting.searchTag)) { // matchの中身 -> /^Tag$/i
+            this.chkFlag = 1;
+          }
+        }
       }
     },
     // 単純一致で比較(キーワード・検索対象)
     partComp: function(targetLtr) {
-      if (typeof targetLtr !== "undefined" && this.clickObj.indexOf(targetLtr) >= 0 && this.blcFlag === 0) {
-        this.blcFlag = 1;
-      } else if (this.chkFlag === 0 && this.clickObj.indexOf(firstSetting.searchKeyword) >= 0) {
-        this.chkFlag = 1;
+      // ブロック処理
+      if (typeof targetLtr !== "undefined") {
+        // 厳密に比較
+        if (this.blcFlag === 0 && GM_getValue("strictComp_flag") === true && this.clickObj.indexOf(targetLtr) >= 0) {
+          this.blcFlag = 1;
+        }
+        // 全角半角・大文字小文字を気にしない
+        else if (this.blcFlag === 0 && GM_getValue("strictComp_flag") === false) {
+          this.clickObj = this.clickObj.toOneByteAlphaNumeric();
+          targetLtr = new RegExp(preg_quote(targetLtr.toOneByteAlphaNumeric(), "/"), "i");
+          if (this.clickObj.match(targetLtr)) {
+            this.blcFlag = 1;
+          }
+        }
+      }
+      // 検索対象が含まれているかのチェック
+      else {
+        // 厳密に比較
+        if (this.chkFlag === 0 && GM_getValue("strictComp_flag") === true && this.clickObj.indexOf(firstSetting.searchKeyword) >= 0) {
+          this.chkFlag = 1;
+        }
+        // 全角半角を気にしない
+        else if (this.chkFlag === 0 && GM_getValue("strictComp_flag") === false) {
+          this.clickObj = this.clickObj.toOneByteAlphaNumeric();
+          if (this.clickObj.match(firstSetting.searchKeyword)) { // matchの中身 -> /Keyword/i
+            this.chkFlag = 1;
+          }
+        }
       }
     },
     // タイトルと記述での処理
@@ -138,6 +199,7 @@ $(function () {
             break;
         }
       } else {
+        console.log("a");
         $(liTag).remove();
       }
     },
@@ -158,7 +220,9 @@ $(function () {
     },
     // ブロックする文字列を削除
     delBlcLtr: function(targetLtr, stackName) {
-      this.blcLtrTemp = firstSetting.blcNameArray[stackName][0].filter(function (theLetter) { return (theLetter !== targetLtr); });
+      this.blcLtrTemp = firstSetting.blcNameArray[stackName][0].filter(function (theLetter) {
+        return (theLetter !== targetLtr);
+      });
       this.blcLtrTemp = this.blcLtrTemp.join(",/,/");
       GM_setValue(stackName, this.blcLtrTemp);
       location.reload();
@@ -182,6 +246,32 @@ $(function () {
     }
   };
 
+  // A JavaScript equivalent of PHP's preg_quote
+  function preg_quote(str, delimiter) {
+    //  discuss at: http://phpjs.org/functions/preg_quote/
+    // original by: booeyOH
+    // improved by: Ates Goral (http://magnetiq.com)
+    // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // improved by: Brett Zamir (http://brett-zamir.me)
+    // bugfixed by: Onno Marsman
+    //   example 1: preg_quote("$40");
+    //   returns 1: '\\$40'
+    //   example 2: preg_quote("*RRRING* Hello?");
+    //   returns 2: '\\*RRRING\\* Hello\\?'
+    //   example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+    //   returns 3: '\\\\\\.\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:'
+
+    return String(str)
+      .replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
+  }
+
+  // 全角英数字文字列を半角文字列に変換する
+  String.prototype.toOneByteAlphaNumeric = function () {
+    return this.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s) {
+      return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    });
+  };
+
   // インスタンス化
   firstSetting = new DBInit();
   btnSetting = new ButtonSet();
@@ -196,96 +286,98 @@ $(function () {
 
   // 検索対象を格納
   if(location.href.match(/www\.nicovideo\.jp\/search\//)) {
-    firstSetting.searchKeyword = $("div.message > p > span:eq(0)").text();
+    firstSetting.searchKeyword = GM_getValue("strictComp_flag") === true ? $("div.message > p > span:eq(0)").text() : new RegExp(preg_quote($("div.message > p > span:eq(0)").text().toOneByteAlphaNumeric(), "/"), "i");
   } else {
-    firstSetting.searchTag = $.trim($("header.contentHeader > h1 > span").text()); // 末尾に空白があるためtrimする
+    firstSetting.searchTag = GM_getValue("strictComp_flag") === true ? $.trim($("header.contentHeader > h1 > span").text()) : new RegExp("^" + preg_quote($.trim($("header.contentHeader > h1 > span").text()).toOneByteAlphaNumeric(), "/") + "$", "i");
   }
+
   // ignoreCacheの設定
   firstSetting.ignoreFlag = GM_getValue("cacheLoad_flag") === true ? false : true;
 
   // 各タグとタイトル格納+ブロック処理
   $("li[data-enable-uad='1']").each(function () {
     var thisLi = $(this); // ローカル変数にしないと最初のタグしか取れず値が変わらない
-
     // HTTPリクエスト
     GM_xmlhttpRequest({
       method: "GET",
       url: "http://ext.nicovideo.jp/api/getthumbinfo/" + $(thisLi).attr("data-id"),
       ignoreCache: firstSetting.ignoreFlag, // falseでキャッシュを使った読み込み
-
       onload: function (responseDetails) {
-            // xmlをパース
+            // 文字列からXMLをパース
         var parser = new DOMParser(),
             doc = parser.parseFromString(responseDetails.responseText, "application/xml");
 
-        // 値をリセット
-        loadFeatures.allTags = [];
-        loadFeatures.blcFlag = 0;
-        loadFeatures.chkFlag = 0;
-        // 動画が削除されていない時
-        if ($(doc).find("nicovideo_thumb_response").attr("status") === "ok") {
-          // 各<tag>タグに対して処理を行う
-          $(doc).find("tag").each(function () {
-            loadFeatures.clickObj = $(this).text();
-            // 検索対象が含まれているか
-            if (typeof firstSetting.searchKeyword !== "undefined") {
-              loadFeatures.partComp();
-            } else {
-              loadFeatures.completeComp();
-            }
-            // ブロックリストと比較＋タグを配列に格納
-            if (typeof GM_getValue("blc_tags") !== "undefined" && firstSetting.blcNameArray["blc_tags"][0][0] !== "" && GM_getValue("blcTags_flag") === true) {
-              if (loadFeatures.blcFlag === 0) {
-                firstSetting.blcNameArray["blc_tags"][0].forEach(loadFeatures.completeComp, loadFeatures);
-              }
-              if (loadFeatures.blcFlag === 0) {
-                loadFeatures.allTags.push("<span class='addBlcTag'>" + loadFeatures.clickObj + "</span>");
-              }
-            } else {
-              loadFeatures.allTags.push("<span class='addBlcTag'>" + loadFeatures.clickObj + "</span>");
-            }
-          });
-          loadFeatures.pushOrRmv("tags", thisLi);
-          if (loadFeatures.blcFlag === 0) {
-            // <title>タグに対して処理を行う
-            $(doc).find("title").each(function () {
-              loadFeatures.clickObj = $(this).text();
-              loadFeatures.ttlAndDscrpt();
-            });
-            loadFeatures.pushOrRmv("titles", thisLi);
-          }
-          if (loadFeatures.blcFlag === 0) {
+        if (responseDetails.status === 200) {
+          // 値をリセット
+          loadFeatures.allTags = [];
+          loadFeatures.blcFlag = 0;
+          loadFeatures.chkFlag = 0;
+          // 動画が削除されていない時
+          if ($(doc).find("nicovideo_thumb_response").attr("status") === "ok") {
             // <description>タグに対して処理を行う
-            $(doc).find("description").each(function () {
-              loadFeatures.clickObj = $(this).text();
-              loadFeatures.ttlAndDscrpt();
-            });
+            loadFeatures.clickObj = $(doc).find("description").text();
+            loadFeatures.ttlAndDscrpt();
             loadFeatures.pushOrRmv("descriptions", thisLi);
+            if (loadFeatures.blcFlag === 0) {
+              // 各<tag>タグに対して処理を行う
+              $(doc).find("tag").each(function () {
+                loadFeatures.clickObj = $(this).text();
+                // 検索対象が含まれているか
+                if (typeof firstSetting.searchTag !== "undefined") {
+                  loadFeatures.completeComp();
+                } else {
+                  loadFeatures.partComp();
+                }
+                // ブロックリストの各キーワードと比較
+                if (typeof GM_getValue("blc_keywords") !== "undefined" && firstSetting.blcNameArray["blc_keywords"][0][0] !== "" && GM_getValue("blcKeywords_flag") === true && loadFeatures.blcFlag === 0) {
+                  firstSetting.blcNameArray["blc_keywords"][0].forEach(loadFeatures.partComp, loadFeatures);
+                }
+                if (loadFeatures.blcFlag === 0) {
+                  // ブロックリストの各タグと比較＋タグを配列に格納
+                  if (typeof GM_getValue("blc_tags") !== "undefined" && firstSetting.blcNameArray["blc_tags"][0][0] !== "" && GM_getValue("blcTags_flag") === true) {
+                    firstSetting.blcNameArray["blc_tags"][0].forEach(loadFeatures.completeComp, loadFeatures);
+                    if (loadFeatures.blcFlag === 0) {
+                      loadFeatures.allTags.push("<span class='addBlcTag'>" + loadFeatures.clickObj + "</span>");
+                    }
+                  } else {
+                    loadFeatures.allTags.push("<span class='addBlcTag'>" + loadFeatures.clickObj + "</span>");
+                  }
+                }
+              });
+              loadFeatures.pushOrRmv("tags", thisLi);
+            }
+            if (loadFeatures.blcFlag === 0) {
+              // <title>タグに対して処理を行う
+              loadFeatures.clickObj = $(doc).find("title").text();
+              loadFeatures.ttlAndDscrpt();
+              loadFeatures.pushOrRmv("titles", thisLi);
+            }
+            // 検索対象外の動画の処理
+            if (loadFeatures.blcFlag === 0 && loadFeatures.chkFlag === 0) {
+              if (GM_getValue("unwantedVid_flag") === false) {
+                $("<br><span style='color:red;'>検索対象を含まない動画です</span>").appendTo($(thisLi).children("p.itemTime"));
+              } else {
+                $(thisLi).remove();
+              }
+            }
           }
-          // 検索対象外の動画の処理
-          if (loadFeatures.blcFlag === 0 && loadFeatures.chkFlag === 0) {
-            if (GM_getValue("unwantedVid_flag") === false) {
-              $("<br><span style='color:red;'>検索対象を含まない動画です</span>").appendTo($(thisLi).children("p.itemTime"));
+          // 削除済み動画の処理
+          else {
+            if (GM_getValue("removedVid_flag") === false) {
+              loadFeatures.aTag = $(thisLi).children("div.itemContent").children("p.itemTitle").children("a");
+              loadFeatures.tagsList[$(thisLi).attr("data-id")] = [0];
+              loadFeatures.titlesList[$(thisLi).attr("data-id")] = $(loadFeatures.aTag).text();
+              $(loadFeatures.aTag).css("text-decoration", "line-through").css("color", "#BEBEBE");
             } else {
               $(thisLi).remove();
             }
           }
-        }
-        // 削除済み動画の処理
-        else {
-          if (GM_getValue("removedVid_flag") === false) {
-            loadFeatures.aTag = $(thisLi).children("div.itemContent").children("p.itemTitle").children("a");
-            loadFeatures.tagsList[$(thisLi).attr("data-id")] = [0];
-            loadFeatures.titlesList[$(thisLi).attr("data-id")] = $(loadFeatures.aTag).text();
-            $(loadFeatures.aTag).css("text-decoration", "line-through").css("color", "#BEBEBE");
-          } else {
-            $(thisLi).remove();
-          }
+        } else {
+          loadFeatures.errChecker(responseDetails);
         }
       },
-      // エラー(サーバが見つからない時)
-      onerror: function (res) {
-        alert("エラーが発生しました。" + "\nレスポンスの本体: " + res.responseText + "\nリクエストの状態: " + res.readyState + "\nレスポンスに含まれるHTTPヘッダー: " + res.responseHeaders + "\nレスポンスのHTTPエラーコード: " + res.status + "\nHTTPステータステキスト: " + res.statusText + "\nリダイレクト先のURL: " + res.finalUrl);
+      onerror: function (responseDetails) {
+        loadFeatures.errChecker(responseDetails);
       }
     });
   });
